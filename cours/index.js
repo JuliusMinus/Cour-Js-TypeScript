dac384d0b23966606dc5d1ba970d336a2439775a
JulienPetit <julienpetit1@gmail.com>
2022-09-12 23:11:28 +0200

    end

--- cours/index.js
+++ cours/index.js
@@ -1,31 +1,35 @@
-// canvas
+// -----------canvas------------------
 
 
-function draw() {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+// function draw() {
+//   const canvas = document.getElementById("canvas");
+//   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect(50, 50, 50, 150);
+//   ctx.fillStyle = "rgb(200,0,0)";
+//   ctx.fillRect(50, 50, 50, 150);
 
-  ctx.fillStyle = "rgba(0,100,200,0.5)";
-  ctx.fillRect(100, 30, 100, 50);
+//   ctx.fillStyle = "rgba(0,100,200,0.5)";
+//   ctx.fillRect(100, 30, 100, 50);
 
-  ctx.fillStyle = "rgba(100,100,200,0.6)";
-  ctx.fillRect(10, 25, 100, 100);
-  ctx.clearRect(35, 25, 40, 40);
-  ctx.strokeRect(125, 75, 50, 60);
+//   ctx.fillStyle = "rgba(100,100,200,0.6)";
+//   ctx.fillRect(10, 25, 100, 100);
+//   ctx.clearRect(35, 25, 40, 40);
+//   ctx.strokeRect(125, 75, 50, 60);
 
 
-ctx.fillStyle = "rgba(400,100,200,0.8)";
-ctx.beginPath();
-ctx.moveTo(180,150)
-ctx.lineTo(100,75)
-ctx.lineTo(100,175)
-ctx.fill()
+// ctx.fillStyle = "rgba(400,100,200,0.8)";
+// ctx.beginPath();
+// ctx.moveTo(180,150)
+// ctx.lineTo(100,75)
+// ctx.lineTo(100,175)
+// ctx.fill()
 
-}
+// }
 
-window.addEventListener("load", draw);
+// window.addEventListener("load", draw);
+
+// ----------------try / catch-----------------------
+
+
 
 
